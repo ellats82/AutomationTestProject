d46233117ba6d52af5aa5d918021c871a9564cc7
Alex Drubezki <alexdru81@gmail.com>
2023-10-02 16:34:01 +0300

    2.10.23

--- src/test/java/Demoblaze.java
+++ src/test/java/Demoblaze.java
@@ -1,6 +1,7 @@
 
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
@@ -8,71 +9,168 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Demoblaze {
 
-    @FindBy(xpath = "//li[@id=\"field_5_2\"]/div/span")
-    private WebElement actualPrice;
+    @FindBy(xpath = "//*[@id='contcont']")
+    private WebElement contcont;
 
-    @FindBy(xpath = "//li[@id=\"field_5_2\"]/div/span")
-    private WebElement priceAfterDelivery;
+    @FindBy(xpath = "//*[@id='tbodyid']")
+    private WebElement tbodyid;
+
+    @FindBy(xpath = "//*[@id='navbarExample']/ul/li[1]/a")
+    private WebElement homeLink;
+
+    @FindBy(id = "nava")
+    private WebElement homeTitle;
+
+    @FindBy(xpath = "//*[@id='navbarExample']/ul/li[2]/a")
+    private WebElement contactLink;
+
+    @FindBy(xpath = "//*[@id=\"exampleModalLabel\"]")
+    private WebElement contactModalTitle;
+
+    @FindBy(xpath = "//*[@id='navbarExample']/ul/li[3]/a")
+    private WebElement aboutUstLink;
+
+    @FindBy(xpath = "//*[@id='videoModalLabel']")
+    private WebElement aboutModalTitle;
+
+    @FindBy(xpath = "//*[@id=\"cartur\"]")
+    private WebElement cartLink;
+
+    @FindBy(xpath = "//*[@id=\"page-wrapper\"]/div/div[1]/h2")
+    private WebElement cartPageTitle;
+
+    @FindBy(id = "login2")
+    private WebElement logInLink;
+
+    @FindBy(id = "logInModalLabel")
+    private WebElement logInModalTitle;
+
+    @FindBy(id = "signin2")
+    private WebElement signUpLink;
+
+    @FindBy(id = "signInModalLabel")
+    private WebElement signUpModalTitle;
+
+    @FindBy(id = "recipient-email")
+    private WebElement emailInput;
+
+    @FindBy(id = "recipient-name")
+    private WebElement nameInput;
+
+    @FindBy(id = "message-text")
+    private WebElement messageText;
+
+    @FindBy(xpath = "//*[@id=\"exampleModal\"]/div/div/div[3]/button[2]")
+    private WebElement contactSubmitButton;
+
+    @FindBy(xpath = "//*[@id=\"tbodyid\"]/div[1]/div/div/h4/a")
+    private WebElement appleMonitorSelectLink;
+
+    @FindBy(xpath = "//*[@id=\"tbodyid\"]/div[2]/div/a")
+    private WebElement addToCartButton;
 
 
-    @FindBy(xpath = "//input[@id=\"input_5_22_3\"]")
-    private WebElement firstName;
 
-    @FindBy(xpath = "//input[@id=\"input_5_22_6\"]")
-    private WebElement lastName;
-
-    @FindBy(xpath = "//select[@id=\"input_5_21\"]")
-    private WebElement isThisPickUpOrDeliverySelect;
-
-    @FindBy(xpath = "//li[@id=\"field_5_19\"]/iframe")
-    private WebElement iframe;
-
-    @FindBy(id = "coupon_Number")
-    private WebElement couponNum;
-
-    @FindBy(xpath = "//textarea[@id=\"input_5_20\"]")
-    private WebElement textarea;
-
-    @FindBy(id = "gform_submit_button_5")
-    private WebElement submitButton;
-
-
-    public String getActualPrice() {
-       return this.actualPrice.getText();
+    public WebElement getPhones() {
+        return this.contcont.findElement(By.xpath("//*[text()='Phones']"));
+    }
+    public WebElement getLaptops() {
+        return this.contcont.findElement(By.xpath("//*[text()='Laptops']"));
+    }
+    public WebElement getMonitors() {
+        return this.contcont.findElement(By.xpath("//*[text()='Monitors']"));
     }
 
-    public void Step03InsertingFirstAndLastName(String firstName, String lastName) {
-        this.firstName.sendKeys(firstName);
-        this.lastName.sendKeys(lastName);
-        System.out.println("First And Last Name : " + "" + firstName + " " + lastName);
-
+    public void clickOnMonitors() {
+        WebElement Monitors = this.getMonitors();
+        Monitors.click();
+        System.out.println("Monitors Button click");
     }
 
-    public void Step04ComboBoxDelivery(String selctedValue) {
-        Select isThisPickUpOrDelivery = new Select(isThisPickUpOrDeliverySelect);
-        isThisPickUpOrDelivery.selectByValue(selctedValue);
-        WebElement option = isThisPickUpOrDelivery.getFirstSelectedOption();
-        System.out.println("Is This Pick Up or Delivery? : " + "" + option.getText());
-    }
-    public String getPriceAfterDelivery() {
-        return this.priceAfterDelivery.getText();
-    }
-
-    public WebElement getIframe() {
-        return this.iframe;
-    }
-
-    public String getCouponNum() {
-        return this.couponNum.getText();
+    public boolean checkIfProductArrExist (String[] productNames){
+        boolean isFound = false;
+        for (int i=0; i<productNames.length; i++){
+            WebElement element = this.tbodyid.findElement(By.xpath("//*[text()='"+productNames[i]+"']"));
+            isFound = element.isDisplayed();
+            System.out.println(productNames[i] + " is found " + isFound);
+            if(!isFound){
+                break;
+            }
+        }
+        return isFound;
     }
 
-    public WebElement getTextarea() {
-        return this.textarea;
+    public WebElement getHomeLink() {
+        return this.homeLink;
     }
 
-    public void Step07ButtonSubmit() {
-        submitButton.click();
-        System.out.println("Submit your order button click");
+    public String getHomeTitle(){
+        return this.homeTitle.getText();
+    }
+
+    public WebElement getContactLink() {
+        return this.contactLink;
+    }
+
+    public WebElement getContactModalTitleElem (){
+        return this.contactModalTitle;
+    }
+
+    public WebElement getAboutUstLink() {
+        return this.aboutUstLink;
+    }
+
+    public WebElement getAboutModalTitle() {
+        return this.aboutModalTitle;
+    }
+
+    public WebElement getCartLink() {
+        return this.cartLink;
+    }
+
+    public WebElement getCartPageTitle() {
+        return this.cartPageTitle;
+    }
+
+    public WebElement getLogInLink() {
+        return this.logInLink;
+    }
+
+    public WebElement getLogInModalTitle() {
+        return this.logInModalTitle;
+    }
+
+    public WebElement getSignUpLink() {
+        return this.signUpLink;
+    }
+
+    public WebElement getSignUpModalTitle() {
+        return this.signUpModalTitle;
+    }
+
+    public WebElement getEmailInput() {
+        return this.emailInput;
+    }
+
+    public WebElement getNameInput() {
+        return nameInput;
+    }
+
+    public WebElement getMessageText() {
+        return messageText;
+    }
+
+    public void clickOnContactSubmitButton() {
+        this.contactSubmitButton.click();
+        System.out.println("contact Submit Button click");
+    }
+
+    public void clickOnAppleMonitorSelectLink() {
+        this.appleMonitorSelectLink.click();
+    }
+
+    public void clickOnAddToCartButton() {
+        this.addToCartButton.click();
     }
 
 
--- src/test/java/DemoblazeSiteTesting.java
+++ src/test/java/DemoblazeSiteTesting.java
@@ -1,16 +1,18 @@
 import com.google.common.util.concurrent.Uninterruptibles;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.w3c.dom.Document;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
@@ -23,6 +25,7 @@ public class DemoblazeSiteTesting {
     String pageUrl = "https://www.demoblaze.com/";
 
     Demoblaze demoblazeObject;
+
 
     @BeforeClass
     public void startSession() {
@@ -37,34 +40,194 @@ public class DemoblazeSiteTesting {
 
     @Test()
     public void Test01Phones() {
-        WebElement contcont = driver.findElement(By.xpath("//*[@id='contcont']"));
-        WebElement Phones = contcont.findElement(By.xpath("//*[text()='Phones']"));
-       // System.out.println("Phones :" + "" + Phones);
+        WebElement Phones = demoblazeObject.getPhones();
+        System.out.println("Phones :" + "" + Phones.getText());
         Phones.click();
-         assertTrue(Phones.isDisplayed());
+
+        String []names={"Samsung galaxy s6","Nokia lumia 1520"};
+        assertTrue(demoblazeObject.checkIfProductArrExist(names));
     }
 
     @Test()
     public void Test01Laptops() {
-        WebElement contcont = driver.findElement(By.xpath("//*[@id='contcont']"));
-        WebElement Laptops = contcont.findElement(By.xpath("//*[text()='Laptops']"));
-        // System.out.println("Laptops :" + "" + Laptops);
+        WebElement Laptops = demoblazeObject.getLaptops();
+        System.out.println("Laptops :" + "" + Laptops.getText());
         Laptops.click();
-        assertTrue(Laptops.isDisplayed());
+
+        String []names={"Sony vaio i5","MacBook air"};
+        assertTrue(demoblazeObject.checkIfProductArrExist(names));
     }
 
     @Test()
     public void Test01Monitors() {
-        WebElement contcont = driver.findElement(By.xpath("//*[@id='contcont']"));
-        WebElement Monitors = contcont.findElement(By.xpath("//*[text()='Monitors']"));
-        // System.out.println("Monitors :" + "" + Monitors);
+        WebElement Monitors = demoblazeObject.getMonitors();
+        System.out.println("Monitors :" + "" + Monitors.getText());
         Monitors.click();
-        assertTrue(Monitors.isDisplayed());
+
+        String []names={"Apple monitor 24","ASUS Full HD"};
+        assertTrue(demoblazeObject.checkIfProductArrExist(names));
     }
 
-    @AfterClass
+    @Test()
+    public void Test02Home() {
+        WebElement homeLink = demoblazeObject.getHomeLink();
+        homeLink.click();
+        System.out.println("linkText: " + homeLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        String title = demoblazeObject.getHomeTitle();
+        System.out.println("title: " + title);
+        Assert.assertTrue(title.contains("PRODUCT STORE"));
+    }
+
+    @Test()
+    public void Test02Contact() {
+        WebElement contactLink = demoblazeObject.getContactLink();
+        contactLink.click();
+        System.out.println("linkText: " + contactLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        WebElement modalTitle = demoblazeObject.getContactModalTitleElem();
+        System.out.println("modalTitle: " + modalTitle.getText());
+        Assert.assertTrue(modalTitle.isDisplayed());
+    }
+
+    @Test()
+    public void Test02AboutUs() {
+        WebElement aboutUstLink = demoblazeObject.getAboutUstLink();
+        aboutUstLink.click();
+        System.out.println("linkText: " + aboutUstLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        WebElement modalTitle =demoblazeObject.getAboutModalTitle();
+        System.out.println("modalTitle: " + modalTitle.getText());
+        Assert.assertTrue(modalTitle.isDisplayed());
+    }
+
+    @Test()
+    public void Test02Cart() {
+        WebElement cartLink = demoblazeObject.getCartLink();
+        System.out.println("linkText: " + cartLink.getText());
+        cartLink.click();
+
+        Uninterruptibles.sleepUninterruptibly(4, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+
+        WebElement cartPageTitle = demoblazeObject.getCartPageTitle();
+        System.out.println("PageTitle: " + cartPageTitle.getText());
+
+        Assert.assertEquals(cartPageTitle.getText(),"Products");
+    }
+
+    @Test()
+    public void Test02LogIn() {
+        WebElement logInLink = demoblazeObject.getLogInLink();
+        logInLink.click();
+        System.out.println("linkText: " + logInLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        WebElement logInModalTitle = demoblazeObject.getLogInModalTitle();
+        System.out.println("modalTitle: " + logInModalTitle.getText());
+        Assert.assertTrue(logInModalTitle.isDisplayed());
+    }
+
+    @Test()
+    public void Test02SignUp() {
+        WebElement signUpLink = demoblazeObject.getSignUpLink();
+        signUpLink.click();
+        System.out.println("linkText: " + signUpLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        WebElement signUpModalTitle = demoblazeObject.getSignUpModalTitle();
+        System.out.println("modalTitle: " + signUpModalTitle.getText());
+        Assert.assertTrue(signUpModalTitle.isDisplayed());
+    }
+
+    @Test()
+    public void Test03ContactFields() {
+        WebElement contactLink = demoblazeObject.getContactLink();
+        contactLink.click();
+        System.out.println("linkText: " + contactLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        WebElement emailInput = demoblazeObject.getEmailInput();
+        WebElement nameInput = demoblazeObject.getNameInput();
+        WebElement messageText = demoblazeObject.getMessageText();
+
+        Assert.assertTrue(emailInput.isDisplayed() && nameInput.isDisplayed() && messageText.isDisplayed() );
+    }
+
+    @Test()
+    public void Test03ContactSendMessage() {
+        WebElement contactLink = demoblazeObject.getContactLink();
+        contactLink.click();
+        System.out.println("linkText: " + contactLink.getText());
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+
+        WebElement emailInput = demoblazeObject.getEmailInput();
+        WebElement nameInput = demoblazeObject.getNameInput();
+        WebElement messageText = demoblazeObject.getMessageText();
+        emailInput.sendKeys("ella@gmail.com");
+        nameInput.sendKeys("ella");
+        messageText.sendKeys("sucsses contact test");
+        demoblazeObject.clickOnContactSubmitButton();
+
+        Alert alert = driver.switchTo().alert();
+        System.out.println("Alert text is: " + alert.getText());
+
+        Assert.assertEquals(alert.getText(),"Thanks for the message!!");
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        alert.accept();
+
+    }
+
+    @Test()
+    public void Test04ChoseProducts() {
+        demoblazeObject.clickOnMonitors();
+
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        demoblazeObject.clickOnAppleMonitorSelectLink();
+
+        Uninterruptibles.sleepUninterruptibly(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        demoblazeObject.clickOnAddToCartButton();
+
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        Alert alert = driver.switchTo().alert();
+        System.out.println("Alert text is: " + alert.getText());
+
+        Assert.assertEquals(alert.getText(),"Product added");
+        Uninterruptibles.sleepUninterruptibly(4, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        alert.accept();
+
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        WebElement homeLink = demoblazeObject.getHomeLink();
+        System.out.println("linkText: " + homeLink.getText());
+        homeLink.click();
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        demoblazeObject.clickOnMonitors();
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        demoblazeObject.clickOnAppleMonitorSelectLink();
+
+//        wait till page loaded
+        Uninterruptibles.sleepUninterruptibly(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+        @AfterClass
     public void closeSession() {
-        //CloseBrowserTest10
+
         Uninterruptibles.sleepUninterruptibly(4, TimeUnit.SECONDS);
         driver.quit();
 
